0893a6539d5efb42b87dd220f347119d031b1679
Roberto Jiménez <sougaradh@gmail.com>
2024-02-16 23:50:10 -0600

    Initialize

---
+++ README.md
@@ -0,0 +1,13 @@
+# Unitec
+
+Landing page de la empresa de servicios de transporte Unitec.
+
+## Author
+
+[@RoberthDaraghouz](https://www.github.com/RoberthDaraghouz)
+
+## 🔗 Links
+
+[![portfolio](https://img.shields.io/badge/roberto_jimenez_dev-333333?style=for-the-badge&logo=ko-fi&logoColor=white)](https://www.robertojimenezdev.com/)
+
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/roberto-jimenez-dev/)
---
+++ js/main.js
@@ -0,0 +1,31 @@
+(function () {
+  var counter = function () {
+    $(".js-counter").countTo({
+      formatter: function (value, options) {
+        return value.toFixed(options.decimals);
+      },
+    });
+  };
+
+  var counterWayPoint = function () {
+    if ($("#prepago").length > 0) {
+      $("#prepago").waypoint(
+        function (direction) {
+          if (direction === "down" && !$(this.element).hasClass("animated")) {
+            setTimeout(counter, 400);
+            $(this.element).addClass("animated");
+          }
+        },
+        { offset: "90%" }
+      );
+    }
+  };
+
+  $(function () {
+    new WOW().init();
+
+    counterWayPoint();
+
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+})();
